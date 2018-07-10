2ac73618039d93266edad88efcfe5dd558c0d67d
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-07-10 06:54:13 -0400

    tues com 1

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,14 +1,44 @@
 
+var heroHealth = 0;
+var villianHealth = 0;
+var heroPicked = false;
+var villianPicked = false;
 
-$(function() {
- 
 
-$(".hero1").click(function(){
-alert("ah you click")
-});
 
-    function hero(){
-        var hero = this
+$(".obiwan").on("click",function(){
+    $(".yoda").hide()
+    heroPicked = true
+})
+
+$(".yoda").on("click",function(){
+    $(".obiwan").hide()
+    heroPicked = True
+})
+
+$(".jarjar").on("click",function(){
+    $(".jabba").hide()
+    $(".maul").hide()
+    villianPicked = true
+})
+
+$(".jabba").on("click",function(){
+    $(".maul").hide()
+    $(".jarjar").hide()
+    villianPicked = true
+})
+
+$(".maul").on("click",function(){
+    $(".jarjar").hide()
+    $(".jabba").hide()
+    villianPicked = true
+})
+
+
+function gameStart(){
+    if (heroPicked && villianPicked){
+        alert("Welcome! Please play wisely")
     }
+}
 
-})();
+
