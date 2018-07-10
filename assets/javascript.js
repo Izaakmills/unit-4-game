823bb4b9d93b7f4064b04b4683871ddc29748a69
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-07-10 16:57:54 -0400

    tue commt 4

--- assets/javascript.js
+++ assets/javascript.js
@@ -5,32 +5,38 @@ var heroPicked = false;
 var villianPicked = false;
 
 
-
+// selects users player and villian for game
 $(".obiwan").on("click",function(){
     $(".yoda").css("display","none")
     heroPicked = true
+    $(".status-bar").html("You have chosen Obi Wan, wise choice.")
 })
 
 $(".yoda").on("click",function(){
     $(".obiwan").css("display","none")
     heroPicked = true
+    $(".status-bar").html("You have chosen Yoda, choose wisely you did.")
 })
 
 $(".jarjar").on("click",function(){
     $(".jabba").css("display","none")
     $(".maul").css("display","none")
+    $(".status-bar").append("<p>You chose to fight jarjar, interesting</p>")
     villianPicked = true
+    
 })
 
 $(".jabba").on("click",function(){
     $(".maul").css("display","none")
     $(".jarjar").css("display","none")
+    $(".status-bar").append("<p>You chose to fight jabba, interesting</p>")
     villianPicked = true
 })
 
 $(".maul").on("click",function(){
     $(".jarjar").css("display","none")
     $(".jabba").css("display","none")
+    $(".status-bar").append("<p>You chose to fight Darth Maul, interesting</p>")
     villianPicked = true
 })
 
