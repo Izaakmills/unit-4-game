dab75727a495a962d5d3926a11a6fb4942500680
i_mills <izaakmills@Izaaks-MacBook-Pro.local>
2018-07-11 17:28:18 -0400

    wed com1

--- assets/javascript.js
+++ assets/javascript.js
@@ -8,45 +8,57 @@ $("#btn-attack").hide()
 
 // selects users player and villian for game
 $(".obiwan").on("click",function(){
-    $(".yoda").css("display","none")
+    $(".yoda-col").css("display","none")
+    $(".goodguypicker").css("display","none")
     $("#btn-attack").show()
-    $(".goodguypicker").css("display","none").css("float","left")
     heroPicked = true
     $(".goodguy-status").html("You have chosen Obi Wan, wise choice.")
 })
 
 $(".yoda").on("click",function(){
-    $(".yoda").css("display","none")
-    $("#btn-attack").show()
-    $(".goodguypicker").css("display","none")
+    $(".yoda-col").css("display","none") //hide yoda section and trasnplant over to obi wan
+    $(".goodguypicker").css("display","none")//hide pick your hero section
     $(".replaceobi").attr("src","https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/Yoda_Attack_of_the_Clones.png/170px-Yoda_Attack_of_the_Clones.png")
+    $(".obi-text").html("Yoda")
+    $("#btn-attack").hide()
     heroPicked = true
     $(".goodguy-status").html("You have chosen Yoda, choose wisely you did.")
 })
-
+// villian entries
 $(".jarjar").on("click",function(){
-    $(".jabba").css("display","none")
-    $(".maul").css("display","none")
+    $(".jarjar-col").css("display","none");
+    $(".jabba-col").css("display","none")
+    $(".maul-col").css("display","none")
     $(".badguypicker").css("display","none")
-    $(".badguy-status").append("<br><p>You chose to fight jarjar, interesting</p>")
+    $(".yoda-col").css("display","")
+    $(".yoda-text").html("jajar")
+    $(".badguy-status").html("<br><p>You chose to fight jarjar, interesting</p>")
     $(".replaceyoda").attr("src","https://vignette.wikia.nocookie.net/starwars/images/d/d2/Jar_Jar_aotc.jpg/revision/latest?cb=20080303052132")
+    // $(".yoda-col").css("display","")
     villianPicked = true
 })
 
 $(".jabba").on("click",function(){
-    $(".maul").css("display","none")
-    $(".jarjar").css("display","none")
-    $(".badguypicker").css("display","none")
-    $(".badguy-status").append("<p>You chose to fight jabba, interesting</p>")
-    $(".replaceyoda").attr("src","http://www2.pictures.zimbio.com/mp/qAXmGpKrxaRl.jpg")
+    $(".maul-col").css("display","none");
+    $(".jarjar-col").css("display","none");
+    $(".jabba-col").css("display","none");
+    $(".badguypicker").css("display","none");
+    $(".yoda-col").css("display","")
+    $(".yoda-text").html("jabba")
+    $(".badguy-status").html("<p>You chose to fight jabba, interesting</p>");
+    $(".replaceyoda").attr("src","http://www2.pictures.zimbio.com/mp/qAXmGpKrxaRl.jpg");
+    $("#yoda-text").html("Jabba");
     villianPicked = true
 })
 
 $(".maul").on("click",function(){
-    $(".jarjar").css("display","none")
-    $(".jabba").css("display","none")
+    $(".jarjar-col").css("display","none")
+    $(".jabba-col").css("display","none")
+    $(".maul-col").css("display","none")
     $(".badguypicker").css("display","none")
-    $(".badguy-status").append("<p>You chose to fight Darth Maul, interesting</p>")
+    $(".yoda-col").css("display","")
+    $(".yoda-text").html("Darth Maul")
+    $(".badguy-status").html("<p>You chose to fight Darth Maul, interesting</p>")
     $(".replaceyoda").attr("src","https://www.sideshowtoy.com/wp-content/uploads/2015/10/star-wars-darth-maul-sixth-scale-feature-100156.jpg")
     villianPicked = true
 })
@@ -63,16 +75,22 @@ function startGame(){
         if (villianHealth <= 0 )
         {
             $(".badguy-status").html("You have defeated your opponent!")
+            call.resetGame()
+            $(".goodguy-status").html("Select another hero to play again")
+
         } else{
         // heroHealth --;
         $(".goodguy-status").html("You attack and...")
         $(".badguy-status").html("<p>Direct hit, for " + hit + " points! They only have " + villianHealth + " health left!</p>")
                 }
-}
-}
+};
+};
 
 function resetGame(){
-    
-}
+    heroPicked = false;
+    villianPicked = false;
+    villianHealth = 50;
+    $("#btn-attack").hide()
+};
 
 
